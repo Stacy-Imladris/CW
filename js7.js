a32bf89e36aeddc3e1d2104ca031863aa24e48ce
stacy <ada.davis27@gmail.com>
2021-11-15 17:30:46 +0300

    new file

--- js7.js
+++ js7.js
@@ -1,10 +1,3 @@
-<p>
-    Hello, my name is
-    <b>Stacy</b>
-    <i>Stacy</i>
-</p>
-<hr />
-<p>
-    function saleHotdogs(n){
-        return n<5 ? n*100 : n>=5 && n<10 ? n*95 : n*90;
-      }
+function saleHotdogs(n){
+    return n<5 ? n*100 : n>=5 && n<10 ? n*95 : n*90;
+}
