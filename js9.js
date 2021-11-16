798b434554b34a4450819afb8da9b10efbcd8e39
stacy <ada.davis27@gmail.com>
2021-11-16 20:16:50 +0300

    changes

--- js9.js
+++ js9.js
@@ -1,18 +1,12 @@
 function padIt(str,n){
-  let x = 1;
-  let y = 1;
+  let i = 1;
   let b;
   let c = n%2;
   if (n>1) {
     b = (n-n%2)/2;
-   
-  while (x<=b){
-    str = "*" + str;
-    x++;
-  }
-  while (y<=b){
-    str = str + "*";
-    y++;
+    while (i<=b){
+    str = "*" + str + "*";
+    i++;
   }
   }
   else {
