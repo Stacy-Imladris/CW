4d2cc34f5b819e584369f22252ff75f98ada6b56
stacy <ada.davis27@gmail.com>
2021-11-21 14:45:36 +0300

    new file

---
+++ trafficligth.js
@@ -0,0 +1,9 @@
+function updateLight(current) {
+  if (current == "green"){
+    return current = "yellow";
+  }
+  else if (current == "yellow"){
+    return current = "red";
+  }
+  else return current = "green";
+}
