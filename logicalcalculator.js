dfcf9738797567ca7611e51856835c1e48115250
stacy <ada.davis27@gmail.com>
2021-12-05 02:50:50 +0300

    new file

---
+++ logicalcalculator.js
@@ -0,0 +1,15 @@
+function logicalCalc(array, op){
+  let result = array[0];
+  for (let i = 1; i < array.length; i++){
+    if (op == "AND"){
+      result = result && array[i];
+    }
+    if (op == "OR"){
+      result = result || array[i];
+    }
+    if (op == "XOR"){
+      result = result != array[i];
+    }
+  }
+  return result;
+}
