aa5f8f6cdd8c17338de9f329970e89b76e47c55b
stacy <ada.davis27@gmail.com>
2021-12-07 00:47:58 +0300

    new file

---
+++ awolfinsheepsclothing.js
@@ -0,0 +1,6 @@
+function warnTheSheep(queue) {
+  if (queue.indexOf("wolf") === queue.length - 1){
+    return "Pls go away and stop eating my sheep";
+  }
+  return "Oi! Sheep number " + ((queue.length - 1) - queue.indexOf("wolf")) + "! You are about to be eaten by a wolf!";
+}
