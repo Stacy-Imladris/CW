a9254369805e83c3175cc50ba432d35271417372
stacy <ada.davis27@gmail.com>
2021-12-23 05:36:56 +0300

    new file

---
+++ shortestword.js
@@ -0,0 +1,10 @@
+function findShort(s){
+  let arr = s.split(' ');
+  let result = arr[0].length;
+  for (let i = 1; i < arr.length; i++){
+    if (arr[i].length < result){
+      result = arr[i].length;
+    }
+  }
+  return result;
+}
