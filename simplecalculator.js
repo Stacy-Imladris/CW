a381b9f1d406868866f573770d931a42011c5103
stacy <ada_davis27@gmail.com>
2022-01-11 00:30:58 +0300

    new file

---
+++ simplecalculator.js
@@ -0,0 +1,10 @@
+function calculator(a,b,sign){
+    if (typeof a === 'number' && typeof b === 'number'){
+      return sign === '+' 
+        ? a + b : sign === '-' 
+        ? a - b : sign === '*' 
+        ? a * b : sign === '/' 
+        ? a / b : "unknown value"
+    }
+    else return "unknown value"
+  }
