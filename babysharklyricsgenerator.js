7e15e57e35199e8f6bee4fe987874f40900152e2
stacy <ada_davis27@gmail.com>
2022-02-02 13:01:43 +0300

    new files

---
+++ babysharklyricsgenerator.js
@@ -0,0 +1,4 @@
+const babySharkLyrics = () => {
+    const f = (arg) => (arg + ', doo doo doo doo doo doo\n').repeat(3) + arg + '!\n'
+    return f('Baby shark') + f('Mommy shark') + f('Daddy shark') + f('Grandma shark') + f('Grandpa shark') + f('Let\'s go hunt') + 'Run away,…'
+  }
