c8b116dc8320f31a88a291326690b90bce990289
stacy <ada.davis27@gmail.com>
2022-02-27 03:59:16 +0300

    new file

---
+++ codingmeetup17.js
@@ -0,0 +1,8 @@
+const sortByLanguage = (list) => {
+    return list.sort((a, b) => {   
+        if (a.language > b.language) return 1;
+        if (a.language < b.language) return -1;
+        if (a.firstName > b.firstName) return 1;
+        if (a.firstName < b.firstName) return -1; 
+      })
+  }
