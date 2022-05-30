69988c1656d6fd1022991f96a92cb50a0956423f
stacy <ada.davis27@gmail.com>
2022-05-31 00:28:52 +0300

    new file

---
+++ stringsstringsstringseasy.js
@@ -0,0 +1,9 @@
+Number.prototype.toString = function() {
+  return JSON.stringify(this)
+}
+Boolean.prototype.toString = function() {
+  return JSON.stringify(this)
+}
+Array.prototype.toString = function() {
+  return JSON.stringify(this)
+}
