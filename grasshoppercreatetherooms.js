119a981e725912d09b6e9fa273e44b996c646870
stacy <ada.davis27@gmail.com>
2022-08-05 00:11:54 +0300

    new file

---
+++ grasshoppercreatetherooms.js
@@ -0,0 +1,17 @@
+const rooms = {
+  name: {
+    room1: 1,
+    room2: 2,
+    room3: 3,
+  },
+  description: {
+    desc1: 'text1',
+    desc2: 'text2',
+    desc3: 'text3'
+  },
+  completed: {
+    comp1: true,
+    comp2: true,
+    comp3: true
+  }
+}
