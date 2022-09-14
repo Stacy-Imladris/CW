e14ade0df723181ae4170a940fa1fc24452d4c7e
stacy <ada.davis27@gmail.com>
2022-09-15 01:44:06 +0300

    new file

---
+++ weneedthesedataimmediately.js
@@ -0,0 +1,11 @@
+let time = Date.now()
+let data = (function returnSomeData(time){
+  return { 
+    name: '__B00013',
+    timeStamp: time || 'unknown',    
+    type: 'raw',
+    var1: 0x041451,
+    var2: 0x00,
+    var3: 0x00040
+  }
+})()
