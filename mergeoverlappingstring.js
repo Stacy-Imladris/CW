4502b4f3bba6209f63a8e41fff1e027cb57fc9c8
stacy <ada.davis27@gmail.com>
2023-05-26 12:51:59 +0300

    changed file

--- mergeoverlappingstring.js
+++ mergeoverlappingstring.js
@@ -1,12 +1,10 @@
 function mergeStrings(first, second){
   if (first.endsWith(second)) return first
-  let matchingLength = 0
-  let i = 1
-  while (!first.endsWith(second.substring(0, i))) {
-    matchingLength = i
-    i++
+  if (!first || !second) return first + second
+  for (let i = 0; i < first.length; i++) {
+    if (second.startsWith(first.substring(i))) {
+      return first.substring(0, i) + second
+    }
   }
-  matchingLength++
-  
-  return first.substring(0, first.length - matchingLength) + second
+  return first + second
 }
